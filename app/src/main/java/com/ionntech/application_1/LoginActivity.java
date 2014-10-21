49c711268c3badfbcea8157fd35e3744afc1544e
Tim <tanda88@gmail.com>
2014-10-21 14:29:32 -0500

    Test change

--- app/src/main/java/com/ionntech/application_1/LoginActivity.java
+++ app/src/main/java/com/ionntech/application_1/LoginActivity.java
@@ -44,7 +44,7 @@ public class LoginActivity extends PlusBaseActivity implements LoaderCallbacks<C
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * TODO: remove after connecting to a real authentication system.  Test
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
